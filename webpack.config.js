0c634fd62bbfa7efead59c7471db601437a81947
molchanov.v <molchanov.v@seleclet.ru>
2023-07-23 14:56:05 +0300

    added sass and html work in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -6,6 +7,25 @@ module.exports = {
     },
     output: {
         filename: "[name].[fullhash].build.js",
-        path: path.resolve(__dirname, './dist')
-    }
+        path: path.resolve(__dirname, './dist'),
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(sc|sa|c)ss$/i,
+                exclude: /(node_modules|bower_components)/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html')
+        })
+    ]
 }
