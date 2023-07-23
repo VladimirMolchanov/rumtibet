6b3aa552a60a7a40f738650a04b1a8a41305f470
molchanov.v <molchanov.v@seleclet.ru>
2023-07-23 19:04:11 +0300

    optimization css config

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    plugins: [
+        require('autoprefixer'),
+        require('css-mqpacker'),
+        require('cssnano')({
+            preset: ['default', {
+                normalizeWhitespace: false
+            }],
+        })
+    ],
+    minimize: false
+}
